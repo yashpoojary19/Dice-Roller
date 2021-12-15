e99b745e083abf53d7461699bdc7921dab60392b
Yash Poojary <yashspoojary245@gmail.com>
2021-12-15 13:37:50 +0530

    Initial Commit

---
+++ Dice Roller/Dice_RollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_RollerApp.swift
+//  Dice Roller
+//
+//  Created by Yash Poojary on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Dice_RollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
